8bc80fdbaa6bebdb200abd8224231ce496a6ef6b
EmmanuelMwangi021 <manumwangi2@gmail.com>
2026-08-01 21:04:06 +0300

    Added the functionality of some props

--- personal-project-showcase-app/src/App.jsx
+++ personal-project-showcase-app/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import SearchBar from './components/SearchBar';
-import ProjectCard from './components/ProjectCard';
+import ProjectList from './components/ProjectList';
+import projectsData from './data/data';
 
 
 function App() {
+    const[projects, setProjects] = useState(projectsData);
+    
     return(
         <div className= "app">
             <Header />
             <ProjectForm />
             <SearchBar />
+            <ProjectList projects= {projects} />
             
         </div>
     );
---
+++ personal-project-showcase-app/src/data/data.js
@@ -0,0 +1,20 @@
+const projectsData = [
+    {
+        id: 1,
+        title: "Portfolio Website",
+        description: "A personal portfolio created using React and Vite"
+    }, 
+
+    {
+        id: 2,
+        title: "Weather App",
+        description: "Displays real-time weather using an API"
+    },
+    
+    {
+        id: 3,
+        title: "Wordly Dictionary",
+        description: "Displays searched words and uses an API"
+    }  
+];
+export default projectsData;
